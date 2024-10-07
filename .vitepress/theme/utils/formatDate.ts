eccad4c26ea153ba8fd4ec0c50b44ba4d791cefb
Syver Døving Agdestein <syverda@gmail.com>
2024-10-07 21:13:12 +0200

    fix: minor fixes

--- .vitepress/theme/utils/formatDate.ts
+++ .vitepress/theme/utils/formatDate.ts
@@ -1,7 +1,8 @@
-export default function ( date: string ) {
-    return new Date( date ).toLocaleString( 'EN', {
+export default function (date: string) {
+    return new Date(date)
+        .toLocaleString( 'NL', {
         year: 'numeric',
         month: '2-digit',
         day: '2-digit'
-    } );
+    });
 }
