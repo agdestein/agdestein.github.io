50f61c841964c5ab05b95a6394e589c7a88962c1
Syver Døving Agdestein <syverda@gmail.com>
2024-10-13 23:17:42 +0200

    add Figure component

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
+import Figure from './components/Figure.vue';
 
 export default {
     extends: DefaultTheme,
+    enhanceApp({ app, router, siteData }) {
+        app.component( 'Figure', Figure );
+    }
 } satisfies Theme
